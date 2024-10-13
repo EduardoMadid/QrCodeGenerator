4ba3cfdbe4c4ad9bafb35264894e1d91219cb606
Eduardo <eduardoocmadid@gmail.com>
2024-10-13 00:57:03 -0300

    Tailwind

--- Site/script.js
+++ Site/script.js
@@ -1,15 +1,24 @@
-let imgBox = document.getElementById('imgBox');
-let qrImage = document.getElementById('qrImage');
-let qrText = document.getElementById('qrText');
+function GenerateQR() {
+    const qrText = document.getElementById("qrText");
+    const qrImage = document.getElementById("qrImage");
+    const imgBox = document.getElementById("imgBox");
 
-function GenerateQR(){
-    if(qrText.value.length > 0){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-    imgBox.classList.add('show-img');
-    } else{
-        qrText.classList.add('error');
-        setTimeout(()=>{
-            qrText.classList.remove('error');
-        },1000);
+    // Remove shake class if it's already present
+    qrText.classList.remove("animate-shake");
+
+    if (qrText.value) {
+        const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrText.value)}&size=200x200`;
+        qrImage.src = qrUrl;
+
+        // Exibe a imagem
+        imgBox.classList.add("max-h-60");
+    } else {
+        // Adiciona a classe de animação de shake
+        qrText.classList.add("animate-shake");
+
+        // Remove a classe após a animação para que possa ser usada novamente
+        setTimeout(() => {
+            qrText.classList.remove("animate-shake");
+        }, 500); // Duração do shake em milissegundos
     }
 }
